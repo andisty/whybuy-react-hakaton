80154d98ab0a061f42da566af3c71fdbd24ce0c0
andisty <distefano.andrea7@gmail.com>
2016-07-15 16:05:38 +0200

    Add items list

---
+++ src/ListItems.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class ListItems extends React.Component {
+  contructor() {
+    super();
+
+    this.state = {
+      name: "Loading...",
+      listItems: []
+    };
+  }
+
+  renderListItems(item, index) {
+    return (
+      <h2>Items list</h2>
+    );
+  }
+
+  render() {
+    let name = this.state.name;
+    let listItems = this.state.listItems;
+
+    return (
+      <div>
+        <h1>{name}</h1>
+        <div>
+          {listItems.map(this.renderItem.bind(this))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ListItems;
