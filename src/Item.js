dea5f6c956db57a99cc519a4ffcd98fa59a81f38
Ward Verhoef <bypandad@gmail.com>
2016-07-16 00:02:57 +0200

    add destroy function to item.js

--- src/Item.js
+++ src/Item.js
@@ -20,8 +20,8 @@ class Item extends React.Component {
       loading: !!!this.props.id
     });
   }
-  // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
+// ############### Update item to create an new item #######################
   updateItem(newItem) {
     console.log(newItem);
     this.syncState({item: newItem});
@@ -51,7 +51,7 @@ class Item extends React.Component {
     this.setState(newState);
 
     console.log(newState);
-
+// adding the new created item to the
     jQuery.ajax({
       type: "PUT",
       url: `https://fierce-brook-27687.herokuapp.com/items/${this.props.id}.json`,
@@ -82,8 +82,36 @@ class Item extends React.Component {
       });
       component.props.onChange();
     });
-}
+  }
 
+// ########DESTROY#############################################################
+  destroyMe(event) {
+    event.preventDefault();
+    console.log("Destroy clicked!");
+
+    let component = this;
+    // delete item from json
+    jQuery.ajax({
+      type: "DELETE",
+      url: `https://fierce-brook-27687.herokuapp.com/items/${this.props.id}.json`,
+      contentType: "application/json",
+      dataType: "json"
+    })
+
+    .done(function(data) {
+      console.log(data);
+      console.log("Deleted! :)");
+    })
+
+    .fail(function(error) {
+      console.log(error);
+    })
+
+    .always(function() {
+      component.props.onDestroy();
+    });
+  }
+  // #####################################################################
     render() {
       console.log(this.state);
         return (
